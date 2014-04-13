ce43fe2c8df37e530e819b7088348bf1d99d78be
scouserTommy <rostyslav.skliar@gmail.com>
2014-04-13 22:29:33 +0300

    properties file for db test

--- service/src/test/java/com/cisco/hibernate/TestPersistenceConfig.java
+++ service/src/test/java/com/cisco/hibernate/TestPersistenceConfig.java
@@ -1,8 +1,10 @@
 package com.cisco.hibernate;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -13,22 +15,34 @@ import java.util.Properties;
  * Time: 20:43
  */
 @Configuration
+@PropertySource("classpath:unitils.properties")
 public class TestPersistenceConfig {
+
+    @Value("${database.driverClassName}")
+    private String driverClassName;
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.userName}")
+    private String user;
+    @Value("${database.password}")
+    private String password;
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-        dataSource.setUrl("jdbc:hsqldb:file:target/hsqldb/test");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
     Properties hibernateProperties() {
         return new Properties() {
             {
-                setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+                setProperty("hibernate.dialect", hibernateDialect);
 
                 setProperty("hibernate.show_sql", "true");
                 setProperty("hibernate.format_sql", "false");
