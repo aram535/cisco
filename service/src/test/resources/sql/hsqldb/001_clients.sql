4188c6de99b77dd7ab3af34da64e2ddfae8729ad
scouserTommy <rostyslav.skliar@gmail.com>
2014-04-14 22:19:54 +0300

    fix for making sql script - Mysql compatible

--- service/src/test/resources/sql/hsqldb/001_clients.sql
+++ service/src/test/resources/sql/hsqldb/001_clients.sql
@@ -1,7 +1,7 @@
-CREATE TABLE client (
-  id         BIGINT            IDENTITY NOT NULL PRIMARY KEY,
+CREATE TABLE CLIENT (
+  id         BIGINT   NOT NULL PRIMARY KEY,
   client_number      VARCHAR(1000)      NOT NULL,
-  name       VARCHAR(1000)              NOT NULL,
+  NAME       VARCHAR(1000)              NOT NULL,
   city       VARCHAR(1000),
   address    VARCHAR(1000)
 );
