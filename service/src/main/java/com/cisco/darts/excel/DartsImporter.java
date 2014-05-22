1903f20b98ede1464adecb55e895f8f75808b889
GordonShamvey <a.i.yatsenko@gmail.com>
2014-05-22 22:56:06 +0300

    File added

---
+++ service/src/main/java/com/cisco/darts/excel/DartsImporter.java
@@ -0,0 +1,12 @@
+package com.cisco.darts.excel;
+
+import java.io.InputStream;
+
+/**
+ * Created by Alf on 20.05.2014.
+ */
+public interface DartsImporter {
+
+	void importDarts(InputStream inputStream);
+
+}
