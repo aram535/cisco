7a856fe488c1251d7fb88b45d36ffdc52cc11bd6
scouserTommy <rostyslav.skliar@gmail.com>
2014-08-12 21:29:41 +0300

    sales cache removed

--- service/src/main/java/com/cisco/config/CachingConfig.java
+++ service/src/main/java/com/cisco/config/CachingConfig.java
@@ -22,17 +22,11 @@ public class CachingConfig implements CachingConfigurer {
         CacheConfiguration ciscoCacheConfiguration = new CacheConfiguration();
         ciscoCacheConfiguration.setName("ciscoCache");
         ciscoCacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
+        ciscoCacheConfiguration.setTimeToLiveSeconds(600);
         ciscoCacheConfiguration.setMaxEntriesLocalHeap(100);
-
-        CacheConfiguration salesCacheConfiguration = new CacheConfiguration();
-        salesCacheConfiguration.setName("salesCache");
-        salesCacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
-        salesCacheConfiguration.setMaxEntriesLocalHeap(10);
-
 
         net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
         config.addCache(ciscoCacheConfiguration);
-        config.addCache(salesCacheConfiguration);
 
         return net.sf.ehcache.CacheManager.newInstance(config);
     }
